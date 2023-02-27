f40dee0531e3a02a92cc34c7ada69c9b56f1b96f
Leandro Fernandez <leandroofernandezz@gmail.com>
2023-02-27 18:07:45 -0300

    add socketio to client and server with strapi lifecyle

--- packages/web/src/App.js
+++ packages/web/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { createSocketConnection, postMessage } from "./api/websocket";
+
+createSocketConnection();
 
 function App() {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const message = e.target[0].value;
+    postMessage(message);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit}>
+        <input type="text" />
+        <button type="submit">post message</button>
+      </form>
     </div>
   );
 }
---
+++ packages/web/src/api/websocket.js
@@ -0,0 +1,35 @@
+import socketIO from "socket.io-client";
+
+const WS_ENDPOINT = process.env.REACT_APP_WS_URL;
+
+let socket;
+
+function startSocketEvents() {
+  if (!socket) {
+    console.error("Socket not initialized");
+  }
+
+  socket.on("connect", () => {
+    console.log("Connected to socket");
+  });
+
+  socket.on("disconnect", () => {
+    console.log("disconnected from server");
+  });
+
+  socket.on("message", (message) => {
+    console.log("message", message);
+  });
+}
+
+export function createSocketConnection() {
+  if (!socket) {
+    socket = socketIO.connect(WS_ENDPOINT);
+
+    startSocketEvents();
+  }
+}
+
+export function postMessage(message) {
+  socket.emit("message", message);
+}
