c3e54a8cf9090f0ba3481ae8f6585738bb296648
emilianodevborn <emiliano.martino@devborn.com>
2023-02-27 13:11:17 -0300

    new project

---
+++ packages/commons/__tests__/commons.test.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const commons = require('..');
+const assert = require('assert').strict;
+
+assert.strictEqual(commons(), 'Hello from commons');
+console.info('commons tests passed');
---
+++ packages/commons/lib/commons.js
@@ -0,0 +1,7 @@
+'use strict';
+
+module.exports = commons;
+
+function commons() {
+  return 'Hello from commons';
+}
