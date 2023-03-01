17d92de662ef7863cbed5fa7139d80057bdd5276
fferr <fedeferreyra1@gmail.com>
2023-03-01 18:26:06 -0300

    Adding reusable library

---
+++ packages/commons/rollup.config.mjs
@@ -0,0 +1,30 @@
+import babel from 'rollup-plugin-babel';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import graphql from "@rollup/plugin-graphql";
+
+export default {
+    input: 'src/index.js',
+    output: [
+        {
+            file: 'dist/my-library.cjs.js',
+            format: 'cjs',
+        },
+        {
+            file: 'dist/my-library.esm.js',
+            format: 'esm',
+        },
+    ],
+    external: ['react', 'react-native'],
+    plugins: [
+        babel({
+            exclude: 'node_modules/**',
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+        }),
+        resolve(),
+        commonjs({
+            include: 'node_modules/**'
+        }),
+        graphql()
+    ],
+};
---
+++ packages/mobile/src/VideosScrollView.js
@@ -0,0 +1,46 @@
+import React, {useRef, useState} from 'react';
+import {ScrollView, StyleSheet, Dimensions, View, SafeAreaView} from 'react-native';
+import ProductDetails from './ProductDetails';
+
+import VideoPlayer from './VideoPlayer';
+import {ApolloClientWrapper, createApolloClient, useVideosQuery} from "commons/src";
+
+// const asd = require('./src/VideoPlayer/quick_slick_ce04033941.mp4');
+// const videos = [asd, asd];
+const {width, height} = Dimensions.get('window');
+const VideosScrollView = () => {
+
+    const {data: response} = useVideosQuery() || {data:[]};
+    const ref = useRef()
+    const [playingVideo, setPlayingVideo] = useState(0);
+    const [soundOn, setSoundOn] = useState(false);
+    const handleVideoChange = event => {
+        const nextVideo = Math.round(
+            event.nativeEvent.contentOffset.y / (event.nativeEvent.contentSize.height / videos.length)
+        );
+
+        setPlayingVideo(nextVideo);
+    };
+
+    console.log(JSON.stringify(response.videos.data[1].attributes.media.data.attributes.url))
+    const videos =[]
+
+    return (
+        <ScrollView ref={ref} onMomentumScrollEnd={handleVideoChange} pagingEnabled bounces={false}>
+            {videos.map((video, index) => (
+                <>
+                    <VideoPlayer
+                        video={video}
+                        isPlaying={index == playingVideo}
+                        index={index}
+                        toggleSoundOn={() => setSoundOn(!soundOn)}
+                        soundOn={soundOn}
+                    />
+                    <ProductDetails/>
+                </>
+            ))}
+        </ScrollView>
+    );
+};
+
+export default VideosScrollView;
